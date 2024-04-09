83bbccc4a03af18de693d98a117b5458f0c434d4
evaldo <evaaldormj@gmail.com>
2024-04-09 17:05:45 -0300

    Feat: ConsultarNotaFiscalPorNumero

--- Controllers/NotaFiscalController.cs
+++ Controllers/NotaFiscalController.cs
@@ -17,5 +17,40 @@ namespace InventarioTelefonia.Controllers
         {
             _context = context;
         }
+
+        [HttpGet]   
+        public IActionResult ConsultarNotasFiscais()
+        {
+            try
+            {
+                var notasBanco = _context.NotasFiscais.ToList();
+
+                return Ok(notasBanco);
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error.Message);
+            }
+        }
+
+        [HttpGet("{numeroNF}")]
+        public IActionResult ConsultarNotaFiscalPorNumero(string numeroNF)
+        {
+            try
+            {
+                var notaBanco = _context.NotasFiscais.Where(nota => nota.NumeroNF == numeroNF);
+
+                if(notaBanco == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(notaBanco);
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error.Message);
+            }
+        }
     }
 }
